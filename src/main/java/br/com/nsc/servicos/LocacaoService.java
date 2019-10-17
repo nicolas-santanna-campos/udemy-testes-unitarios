55366e424b7554d03ebdf8c843d08cc303cbbf10
nicolas.campos <nicolas.campos@unicred.com.br>
2019-10-17 14:20:58 -0300

    Primeiro commit

---
+++ src/main/java/br/com/nsc/servicos/LocacaoService.java
@@ -0,0 +1,34 @@
+package br.com.nsc.servicos;
+
+import static br.com.nsc.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.com.nsc.entidades.Filme;
+import br.com.nsc.entidades.Locacao;
+import br.com.nsc.entidades.Usuario;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
