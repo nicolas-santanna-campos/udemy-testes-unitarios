9efb5ecd3c7d19271a57885be97671f1fdbe18b8
nicolas.campos <nicolas.campos@unicred.com.br>
2019-11-20 08:32:58 -0300

    1 - Definição do que deve ser um teste em: cenário, ação e verificação.

--- src/main/java/br/com/nsc/servicos/LocacaoService.java
+++ src/main/java/br/com/nsc/servicos/LocacaoService.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import br.com.nsc.entidades.Filme;
 import br.com.nsc.entidades.Locacao;
 import br.com.nsc.entidades.Usuario;
+import br.com.nsc.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -29,6 +30,24 @@ public class LocacaoService {
 	}
 
 	public static void main(String[] args) {
+		//Cenário
+		LocacaoService locacaoService = new LocacaoService();
+		Usuario usuario = new Usuario("Usuário 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
 		
+		//Ação
+		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+		
+		//Verificação
+		System.out.println(locacao.getValor() == 5.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
+	/*	
+	Fast
+	Independent
+	Repeatable
+	Self Verifying
+	Timely
+	*/
 }
